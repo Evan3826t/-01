901ec0a5baf2accb81407556a1a5eda73281159f
Evan3826t <evan3826t@gmail.com>
2019-11-28 11:44:57 +0800

    20191128 上午
    add function in base.php

--- 10802-bquiz01-master/base.php
+++ 10802-bquiz01-master/base.php
@@ -4,6 +4,8 @@ $dsn = "mysql:host=localhost;charser=utf8;dbname=db111";
 $pdo = new PDO($dsn, "root", "123");
 session_start();
 
+
+// 資料表要開空值，如果沒開給空值會錯誤
 // 查詢及取得特定條件的全部資料
 // select * from table where id=xxx or aaa=xxx $$ bbb=yyy
 function find($table,...$arg){
@@ -27,13 +29,12 @@ function find($table,...$arg){
         $sql = $sql . "id='" . $arg[0] . "'";
     }
 
-    echo $sql . "<br>";
     return $pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
 }
 
 print_r(find( "admin", 2));
 echo "<br>";
-print_r(find( "admin", ["acc"=>"root", "pw"=>"root"]));
+print_r(find( "admin", ["acc"=>"aaa", "pw"=>"123"]));
 
 function all( $table, ...$arg){
     global $pdo;
@@ -51,7 +52,7 @@ function all( $table, ...$arg){
         $sql = $sql . $arg[1];
     }
 
-    echo $sql . "<br>";
+    echo "<br>" .$sql . "<br>";
     return $pdo->query($sql)->fetchAll();
 }
 
@@ -95,8 +96,74 @@ function nums($table, ...$arg){
     return $pdo->query($sql)->fetchColumn();
 }
 
-echo "<br>資料表筆數<br>";
+echo "<br><br>資料表筆數<br>";
 echo nums("admin");
-echo "<br>資料表筆數加條件<br>";
+echo "<br><br>資料表筆數加條件<br>";
 echo nums("admin",["pw"=>"1234"]);
+
+
+// 簡化 $pdo->query($sql)->fetchAll(); 
+function q($sql){
+    global $pdo;
+    return $pdo->query($sql)->fetchAll();
+}
+
+// 刪除特定 id 或符合條件的資料
+function del($table, ...$arg){
+    global $pdo;
+    $sql = "DELETE FROM $table WHERE ";
+    if(is_array($arg[0])){
+        foreach($arg[0] as $key => $value){
+            $tmp[] = sprintf("`%s`='%s'", $key, $value);
+        }
+
+        $sql = $sql . implode(" && ", $tmp); 
+    }else{
+        $sql = $sql . "`id`=$arg[0]";
+    }
+    echo $sql."<br>";
+    return $pdo->exec($sql);
+}
+
+echo "<br><br>刪除資料指定id<br>";
+del("admin", 5);
+echo "<br><br>刪除資料指定元素<br><br>";
+del("admin", ["acc"=>"bbb"]);
+echo "<br>";
+
+function to( $path){
+    header("location:".$path);
+
+}
+
+// to("https://google.com.tw");
+
+
+function save($table, $data){
+    global $pdo;
+    
+    if(!empty($data['id'])){
+        // update
+        $sql = " UPDATE $table SET ";
+        foreach($data as $key => $value){
+            $tmp[] = sprintf("`%s`='%s'", $key, $value);
+        }
+
+        $sql = $sql . implode(",", $tmp) . "  WHERE  `id`=" . $data['id'];
+    }else{
+        // insert
+        $key="`" . implode("`,`",array_keys($data)) . "`";
+        $value="'" . implode("','",$data) . "'";
+        $sql = " INSERT INTO $table ($key) VALUES ($value)";
+    }
+    echo $sql;
+    // return $pdo->exec($sql);
+}
+
+echo "Save update<br>";
+save("admin",find( "admin", 2));
+echo "<br><br>";
+echo "Save insert<br>";
+$data=[ "acc" => "zxc", "pw" => "456"];
+save("admin", $data);
 ?>
